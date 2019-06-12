2442dfc0268311609e88a35bd494185b0938c3e3
Glendon Philipp <gpbaculio@gmail.com>
2019-06-13 00:14:15 +0800

    added methods

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -13,9 +13,12 @@ function ConvertHandler() {
   this.getUnit = input => input.replace(/[^A-z]/g, ''); // replace numeric chars with nothing
 
   this.getReturnUnit = function (initUnit) {
-    var result;
-
-    return result;
+    if (initUnit === 'L') return 'gal'
+    else if (initUnit === 'gal') return 'L'
+    else if (initUnit === 'lbs') return 'kg'
+    else if (initUnit === 'kg') return 'lbs'
+    else if (initUnit === 'mi') return 'km'
+    else if (initUnit === 'km') return 'mi'
   };
 
   this.spellOutUnit = function (unit) {
@@ -28,9 +31,10 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-
-    return result;
+    if (initUnit === 'L' || initUnit === 'gal') return initNum * galToL;
+    else if (initUnit === 'lbs' || initUnit === 'kg') return initNum * lbsToKg;
+    else if (initUnit === 'mi' || initUnit === 'km') return initNum * miToKm;
+    else return 'Invalid Unit'
   };
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
